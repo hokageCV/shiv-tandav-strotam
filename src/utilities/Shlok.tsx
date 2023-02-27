df56b2783354035468fa2f295d54d1d687f9aefa
hokageCV <chaitanyavaru@gmail.com>
2023-02-27 11:12:00 +0530

    update spacing

--- src/utilities/Shlok.tsx
+++ src/utilities/Shlok.tsx
@@ -18,7 +18,7 @@ export default function Shlok({ first1, first2, second, third, fourth }: ShlokTy
     const pClass234 = blur !== "1" ? `${blurText} ${noSpacingInner}` : `${noSpacingInner}`;
 
     return (
-        <div className="card select-none mx-0.5 my-1 py-1 px-0 w-96 bg-shlokCardBG text-shlokText">
+        <div className="card select-none mx-0.5 my-1 py-1 px-0 w-80 md:w-96 bg-shlokCardBG text-shlokText">
             <div
                 className={`card-body w-full flex xs:flex-col justify-center items-center  md:text-md lg:text-lg  ${noSpacingOuter}`}
             >
